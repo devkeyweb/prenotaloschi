864fd5c64cae7d75fc3e78ca5fee912397a569b9
Admin <admin@iMac-di-Admin.local>
2020-01-27 12:41:25 +0100

    inserimento footer.php e header.php. Bg color per eventi past...

--- calendarloschi.php
+++ calendarloschi.php
@@ -26,39 +26,41 @@ if(isset($_GET['code'])) {
     $scopes = [ Google_Service_Calendar::CALENDAR, Google_Service_Calendar::CALENDAR_READONLY ];
     $client->setScopes($scopes);
 
-    // Redirect to the page where user can create event
-    //header('Location: home.php');
-    //exit();
   }
   catch(Exception $e) {
+    //header('Location: home.php');
     echo $e->getMessage();
     exit();
   }
 } else {
-
+  //header('Location: home.php');
+  //exit();
 }
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="description" content="">
-<meta name="author" content="">
-<meta name="robots" content="noindex, nofollow">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="robots" content="noindex, nofollow">
 
-<link rel="stylesheet" href="css/style.css" />
-<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.1.9/jquery.datetimepicker.min.css" />
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.1.9/jquery.datetimepicker.min.js"></script>
+  <!-- fullcalendar.io css -->
+  <link href='../packages/core/main.css' rel='stylesheet' />
+  <link href='../packages/daygrid/main.css' rel='stylesheet' />
+  <link href='../packages/timegrid/main.css' rel='stylesheet' />
+  <link href='../packages/list/main.css' rel='stylesheet' />
 
+  <!-- bootstrap css -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <style>
 
+  <!-- datetimepicker css -->
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.1.9/jquery.datetimepicker.min.css" />
 
-
-  </style>
+  <!-- custom css -->
+  <link rel="stylesheet" href="css/style.css" />
 
 </head>
 
@@ -122,33 +124,7 @@ if(isset($_GET['code'])) {
 
   <div id="loading">loading...</div>
 
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg static-top">
-    <div class="container">
-      <a class="navbar-brand" href="#"><img src="./img/traslochi-aziende-uffici.png" class="logoHeader" /></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Services</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <?php include "header.php"; ?>
 
   <!-- Page Content -->
   <div class="container">
@@ -159,6 +135,7 @@ if(isset($_GET['code'])) {
       </div>
 
       <div class="col-lg-8 col-xs-12">
+        <!-- fullcalendar.io -->
         <div id='calendar'></div>
       </div>
 
@@ -181,35 +158,24 @@ if(isset($_GET['code'])) {
     </div>
   </div>
 
-  <footer >
-    <!-- Page Content -->
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12 text-center">
-          Copyright 2018 - Privacy - Cookies Policy<br/>
-Loschi traslochi di Luca Loschi - Sede: Via Carlo Marx 95 – 41012 Carpi (MO) -<br/>
-P.IVA: 03495650362 - CF: LSCLCU73S04B819Q - CCIAA/REA: MO 392898 059 698416 340 4227572 - loschitraslochi@yahoo.it<br/><br/>Web application a cura di Key-web marketing
+  <?php include "footer.php"; ?>
 
-        </div>
-      </div>
-    </div>
-  </footer>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-<!-- fullcalendar.io -->
-<link href='../packages/core/main.css' rel='stylesheet' />
-<link href='../packages/daygrid/main.css' rel='stylesheet' />
-<link href='../packages/timegrid/main.css' rel='stylesheet' />
-<link href='../packages/list/main.css' rel='stylesheet' />
-<script src='../packages/core/main.js'></script>
-<script src='../packages/interaction/main.js'></script>
-<script src='../packages/daygrid/main.js'></script>
-<script src='../packages/timegrid/main.js'></script>
-<script src='../packages/list/main.js'></script>
-<script>
+  <!-- Datetimepicker jquery JavaScript -->
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.1.9/jquery.datetimepicker.min.js"></script>
 
+  <!-- fullcalendar.io js -->
+  <script src='../packages/core/main.js'></script>
+  <script src='../packages/interaction/main.js'></script>
+  <script src='../packages/daygrid/main.js'></script>
+  <script src='../packages/timegrid/main.js'></script>
+  <script src='../packages/list/main.js'></script>
+
+  <script>
   document.addEventListener('DOMContentLoaded', function() {
     // fullcalendar.io
     var calendarEl = document.getElementById('calendar');
@@ -227,19 +193,32 @@ P.IVA: 03495650362 - CF: LSCLCU73S04B819Q - CCIAA/REA: MO 392898 059 698416 340 
       businessHours: true, // display business hours
       editable: false,
       dateClick: function(info) {
-        alert('Clicked on: ' + info.dateStr);
-        alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-        alert('Current view: ' + info.view.type);
+        //alert('Clicked on: ' + info.dateStr);
+        //alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+        //alert('Current view: ' + info.view.type);
         // change the day's background color just for fun
         //info.dayEl.style.backgroundColor = 'red';
-        //console.log(" info ",info);
-        
-        if(info.view.type == 'dayGridMonth' || info.view.type == 'basicWeek') {
-          calendar.changeView('timeGridDay', info.dateStr);      
-        }
-      },
-      
+        console.log(" info ",info); 
 
+        var dataEv = info.dateStr;
+          
+        if($("td[data-date='"+dataEv+"']").hasClass('fc-past')){
+          console.log(" PAST "); 
+        }
+
+        if($("td[data-date='"+dataEv+"']").hasClass('fc-today')){
+          console.log(" TODAY "); 
+        }
+
+        if($("td[data-date='"+dataEv+"']").hasClass('fc-future')){
+          console.log(" FUTURE "); 
+          calendar.changeView('timeGridDay', info.dateStr);    
+        }
+
+        //if(info.view.type == 'dayGridMonth' || info.view.type == 'basicWeek') {
+          //calendar.changeView('timeGridDay', info.dateStr);      
+        //}
+      },
       events: "json-events.php",
       loading: function(bool) { 
         if (bool) $('#loading').show(); 
@@ -247,17 +226,26 @@ P.IVA: 03495650362 - CF: LSCLCU73S04B819Q - CCIAA/REA: MO 392898 059 698416 340 
       },
       eventColor: '#ff0000',
       eventRender: function (event, element) {
-        console.log("ev ",event.event.start);
+        var today = formatDate(new Date());
+        var evdate = formatDate(event.event.start);
+        console.log("ev "+evdate+" "+today);
         //var dataToFind = moment(event.event.start).format('YYYY-MM-DD');
-        //$("td[data-date='"+dataToFind+"']").addClass('activeDay');
+        $("td[data-date='"+today+"']").addClass('activeDay');
+        
+        //if($("td[data-date='"+today+"']").hasClass('fc-past')){
+          //info.dayEl.style.backgroundColor = 'red';
+        //}
+
       },
     });
 
     calendar.render();
 
+
     //form inserimento evento
     //form inserimento evento
     //form inserimento evento
+
     // DateTimePicker plugin : http://xdsoft.net/jqplugins/datetimepicker/
     $("#event-start-time, #event-end-time").datetimepicker({ format: 'Y-m-d H:i', minDate: 0, minTime: 0, step: 5, onShow: AdjustMinTime, onSelectDate: AdjustMinTime });
     $("#event-date").datetimepicker({ format: 'Y-m-d', timepicker: false, minDate: 0 });
@@ -322,13 +310,13 @@ P.IVA: 03495650362 - CF: LSCLCU73S04B819Q - CCIAA/REA: MO 392898 059 698416 340 
 
       // Event details
       parameters = {  title: $("#event-title").val(), 
-              event_time: {
-                start_time: $("#event-type").val() == 'FIXED-TIME' ? $("#event-start-time").val().replace(' ', 'T') + ':00' : null,
-                end_time: $("#event-type").val() == 'FIXED-TIME' ? $("#event-end-time").val().replace(' ', 'T') + ':00' : null,
-                event_date: $("#event-type").val() == 'ALL-DAY' ? $("#event-date").val() : null
-              },
-              all_day: $("#event-type").val() == 'ALL-DAY' ? 1 : 0,
-            };
+        event_time: {
+          start_time: $("#event-type").val() == 'FIXED-TIME' ? $("#event-start-time").val().replace(' ', 'T') + ':00' : null,
+          end_time: $("#event-type").val() == 'FIXED-TIME' ? $("#event-end-time").val().replace(' ', 'T') + ':00' : null,
+          event_date: $("#event-type").val() == 'ALL-DAY' ? $("#event-date").val() : null
+        },
+        all_day: $("#event-type").val() == 'ALL-DAY' ? 1 : 0,
+      };
 
       console.log("par",parameters);
 
@@ -371,6 +359,18 @@ P.IVA: 03495650362 - CF: LSCLCU73S04B819Q - CCIAA/REA: MO 392898 059 698416 340 
       this.setOptions({ minTime: 0 });
     else 
       this.setOptions({ minTime: false });
+  }
+
+  function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
   }
 
 
---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer >
+  <!-- Page Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+          Copyright 2018 - Privacy - Cookies Policy<br/>
+          Loschi traslochi di Luca Loschi - Sede: Via Carlo Marx 95 – 41012 Carpi (MO) -<br/>
+          P.IVA: 03495650362 - CF: LSCLCU73S04B819Q - CCIAA/REA: MO 392898 059 698416 340 4227572 - loschitraslochi@yahoo.it<br/><br/>Web application a cura di Key-web marketing
+      </div>
+    </div>
+  </div>
+</footer>
